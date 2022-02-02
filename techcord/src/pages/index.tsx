2bc05fbf7a8e649ed8f394f2f303bc7b1256485b
KRochaS <karinerocha.s@hotmail.com>
2022-02-01 23:26:08 -0300

    index.tsx

---
+++ techcord/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyle } from "../GlobalStyle";
+
+export default function HomePage() {
+    return (
+        <div>
+            <GlobalStyle />
+            <h1>
+                Boas vindas de volta!
+            </h1>
+            <h2> Discord - Tech</h2>
+        </div>
+    )
+}
